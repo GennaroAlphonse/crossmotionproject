9c46f07b6a09e3bc12fbf36b2e8b286592e3f700
GennaroAlphonse <genalfa@gmail.com>
2017-12-04 19:18:15 -0600

    Create ClsArrastres.vb

---
+++ BXP_Cls/ClsArrastres.vb
@@ -0,0 +1,40 @@
+Public Class ClsArrastres
+
+    Public Property vid_num As String
+    Public Property vCARRIER As String
+    Public Property vORIGIN As String
+    Public Property vPOL_CODE As String
+    Public Property vPOL As String
+    Public Property vPOL_CODE_COUNTRY As String
+    Public Property vPOL_COUNTRY As String
+    Public Property vPOL_ZIPCODE As String
+    Public Property vDESTINATION As String
+    Public Property vPOD_CODE As String
+    Public Property vPOD As String
+    Public Property vPOD_CODE_COUNTRY As String
+    Public Property vPOD_COUNTRY As String
+    Public Property vPOD_ZIPCODE As String
+    Public Property vSERVICE As String
+    Public Property vCURRENCY As String
+    Public Property vTOTAL_RATE_SIN As String
+    Public Property vTOTAL_RATE_TAN As String
+    Public Property vSINGLE As String
+    Public Property vTOLLS_SIN As String
+    Public Property vTANDEM As String
+    Public Property vTOLLS_TAN As String
+    Public Property vEQUIPMENT As String
+    Public Property vVALIDITY_FROM As String
+    Public Property vVALIDITY_TO As String
+    Public Property vTT As String
+    Public Property vWEIGHT_SIN As String
+    Public Property vWEIGHT_TAN As String
+    Public Property vFREE_HOURS As String
+    Public Property vCOMMODITY As String
+    Public Property vNAC As String
+    Public Property vVOLUMEN As String
+    Public Property vSUBJECT As String
+    Public Property vNOT_SUBJECT As String
+    Public Property vREMARKS As String
+
+End Class
+
