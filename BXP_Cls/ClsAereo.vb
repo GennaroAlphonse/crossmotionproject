78cc3e3d390b41ce65c3e2e926318ed83a5c6a30
GennaroAlphonse <genalfa@gmail.com>
2017-12-04 19:17:36 -0600

    Create ClsAereo.vb

---
+++ BXP_Cls/ClsAereo.vb
@@ -0,0 +1,48 @@
+Public Class ClsAereo
+
+    Public Property vid_num As String
+    Public Property vAGENT As String
+    Public Property vAEROLINEA As String
+    Public Property vPOL_CODE As String
+    Public Property vPOL As String
+    Public Property vPOL_CODE_COUNTRY As String
+    Public Property vPOL_COUNTRY As String
+    Public Property vPOD_CODE As String
+    Public Property vPOD As String
+    Public Property vPOD_CODE_COUNTRY As String
+    Public Property vPOD_COUNTRY As String
+    Public Property vVIA1 As String
+    Public Property vVIA2 As String
+    Public Property vVIA3 As String
+    Public Property vVIA4 As String
+    Public Property vVIA5 As String
+    Public Property vCURRENCY As String
+    Public Property vMIN As String
+    Public Property vMEN45KG As String
+    Public Property vMAY45KG As String
+    Public Property vMAY100KG As String
+    Public Property vMAY300KG As String
+    Public Property vMAY500KG As String
+    Public Property vMAY1000KG As String
+    Public Property vFSCKG As String
+    Public Property vSSCKG As String
+    Public Property vTAX As String
+    Public Property vHAWB As String
+    Public Property vHANDLING As String
+    Public Property vROUNTING As String
+    Public Property vFREQUENCY As String
+    Public Property vAIRCRAFT As String
+    Public Property vMAXDimXPZA As String
+    Public Property vMAXWeigthXPZA As String
+    Public Property vVALIDITY_FROM As String
+    Public Property vVALIDITY_TO As String
+    Public Property vTT As String
+    Public Property vNAC As String
+    Public Property vRA As String
+    Public Property vCOMMODITY As String
+    Public Property vCOMMITMENT_VOL As String
+    Public Property vSUBJECT As String
+    Public Property vNOT_SUBJECT As String
+    Public Property vREMARKS As String
+
+End Class
