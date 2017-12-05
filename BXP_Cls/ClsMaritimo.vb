9f595cd439a87b60eb45882ea4ab8b3fac310b76
GennaroAlphonse <genalfa@gmail.com>
2017-12-04 19:20:17 -0600

    Create ClsMaritimo.vb

---
+++ BXP_Cls/ClsMaritimo.vb
@@ -0,0 +1,36 @@
+Public Class ClsMaritimo
+
+    Public Property vid_num As String
+    Public Property vAGENT As String
+    Public Property vCARRIER As String
+    Public Property vPOL_CODE As String
+    Public Property vPOL As String
+    Public Property vPOL_CODE_COUNTRY As String
+    Public Property vPOL_COUNTRY As String
+    Public Property vPOD_CODE As String
+    Public Property vPOD As String
+    Public Property vPOD_CODE_COUNTRY As String
+    Public Property vPOD_COUNTRY As String
+    Public Property vVIA1 As String
+    Public Property vVIA2 As String
+    Public Property vVIA3 As String
+    Public Property vCURRENCY As String
+    Public Property v20GP As String
+    Public Property v40GP As String
+    Public Property v40HQ As String
+    Public Property vEQUIPMENT As String
+    Public Property vVALIDITY_FROM As String
+    Public Property vVALIDITY_TO As String
+    Public Property vTT As String
+    Public Property vFREE_DAYS As String
+    Public Property vNAC As String
+    Public Property vCOMMODITY As String
+    Public Property vUN As String
+    Public Property vIMO_SUB As String
+    Public Property vRA As String
+    Public Property vCOMMITMENT_TES As String
+    Public Property vSUBJECT As String
+    Public Property vNOT_SUBJECT As String
+    Public Property vREMARKS As String
+
+End Class
