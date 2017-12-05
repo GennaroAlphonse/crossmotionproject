317860570eadc01224775dd2271bfd8986a22975
GennaroAlphonse <genalfa@gmail.com>
2017-12-04 19:20:49 -0600

    Create ClsTerrestre.vb

---
+++ BXP_Cls/ClsTerrestre.vb
@@ -0,0 +1,38 @@
+Public Class ClsTerrestre
+
+    Public Property vid_num As String
+    Public Property vCARRIER As String
+    Public Property vORIGIN As String
+    Public Property vPOL_CODE As String
+    Public Property vPOL As String
+    Public Property vPOL_CODE_COUNTRY As String
+    Public Property vPOL_COUNTRY As String
+    Public Property vPOL_ZIPCODE As String
+    Public Property vDESTINATION As String
+    Public Property vPOD_CODE As String
+    Public Property vPOD As String
+    Public Property vPOD_CODE_COUNTRY As String
+    Public Property vPOD_COUNTRY As String
+    Public Property vPOD_ZIPCODE As String
+    Public Property vSTOP As String
+    Public Property vSTOP2 As String
+    Public Property vSTOP3 As String
+    Public Property vCURRENCY As String
+    Public Property vTOTALRATE As String
+    Public Property vKM As String
+    Public Property vFSC As String
+    Public Property vFSC_TOTAL As String
+    Public Property vEQUIPMENT As String
+    Public Property vVALIDITY_FROM As String
+    Public Property vVALIDITY_TO As String
+    Public Property vTT As String
+    Public Property vFREE_DAYS As String
+    Public Property vCOMMODITY As String
+    Public Property vNAC As String
+    Public Property vVOLUMEN As String
+    Public Property vSUBJECT As String
+    Public Property vNOT_SUBJECT As String
+    Public Property vREMARKS As String
+    Public Property vFREE_HRS_LOA_UNLOA As String
+
+End Class
